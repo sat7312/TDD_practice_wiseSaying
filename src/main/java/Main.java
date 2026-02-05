f3cc0d3296a7f69762aec8b5e7a60e1f44ac9ae4
sat7312 <sat7312@gmail.com>
2026-02-05 15:48:58 +0900

    출력부 구현 시작

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,32 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
+        lab2();
+    }
+
+    public static void lab2() {
+
+        PrintStream ORIGINAL_OUT = System.out;
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+
+        System.setOut(printStream);
+
+        System.out.println("hihi");
+        System.out.println("byebye");
+        System.out.println("ok");
+
+        System.setOut(ORIGINAL_OUT); //모니터로 출력 복원
+
+        String result = byteArrayOutputStream.toString();
+        System.out.println(result);
+    }
+
+    public static void lab1() {
         Scanner scan = new Scanner("""
                 등록
                 과거에 집착하지 마라.
