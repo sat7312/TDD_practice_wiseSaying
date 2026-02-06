44dbbef1e276ab85e887ba0afd0119c55a3eb0aa
sat7312 <sat7312@gmail.com>
2026-02-06 10:30:07 +0900

    TestUtil 추가

---
+++ src/test/java/TestUtil.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TestUtil {
+
+    public static void run(String input) {
+
+        //입력값
+        Scanner scan = new Scanner(input);
+
+        //명언 프로그램 실행
+
+        //출력값
+    }
+
+    public static Scanner genScanner(String input) {
+        return new Scanner(input);
+    }
+}
