65662a510ffaaf8b1e2e4444546bc0e8d96e2f20
sat7312 <sat7312@gmail.com>
2026-02-06 12:17:09 +0900

    AppTest, App 리팩토링

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,5 +10,19 @@ public class App {
 
     public void run() {
         System.out.println("== 명언 앱 ==");
+        System.out.println("명령) ");
+        String cmd = scanner.nextLine();
+
+        switch (cmd) {
+            case "등록" -> {
+                System.out.print("명언 : ");
+                String saying = scanner.nextLine();
+                System.out.print("작가 : ");
+                String author = scanner.nextLine();
+            }
+            case "종료" -> {
+                return;
+            }
+        }
     }
 }
---
+++ src/test/java/AppTestRunner.java
@@ -0,0 +1,15 @@
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
+public class AppTestRunner {
+
+    public static String run(String input) {
+
+        Scanner sc = TestUtil.genScanner(input + "\n종료");
+
+        ByteArrayOutputStream outputStream = TestUtil.setOutByteArray();
+        new App(sc).run();
+
+        return outputStream.toString();
+    }
+}
